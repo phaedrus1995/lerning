2dbbc078246e9aa9529e95551d43bba4ce6e76f8
Xi Zhang <72356406+phaedrus1995@users.noreply.github.com>
2022-10-25 21:26:03 +0800

    initializing the framwork

---
+++ src/main/java/com/phaedrus/demo/HalloWorldController.java
@@ -0,0 +1,13 @@
+package com.phaedrus.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HalloWorldController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
---
+++ src/test/java/com/phaedrus/demo/DemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.phaedrus.demo;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = com.example.demo.DemoApplication.class)
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
