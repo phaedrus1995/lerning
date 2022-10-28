91849b0d95cb423adc755b96a09391e0296c33c6
Xi Zhang <72356406+phaedrus1995@users.noreply.github.com>
2022-10-28 14:10:19 +0800

    modify the initial status to be false

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
 
 
-INSERT INTO locker (status, customer_number) VALUES (true, '001');
-INSERT INTO locker (status, customer_number) VALUES (true, '002');
-INSERT INTO locker (status, customer_number) VALUES (true, '003');
-INSERT INTO locker (status, customer_number) VALUES (true, '004');
-INSERT INTO locker (status, customer_number) VALUES (true, '005');
-INSERT INTO locker (status, customer_number) VALUES (true, '007');
+INSERT INTO locker (status, customer_number) VALUES (false, '001');
+INSERT INTO locker (status, customer_number) VALUES (false, '002');
+INSERT INTO locker (status, customer_number) VALUES (false, '003');
+INSERT INTO locker (status, customer_number) VALUES (false, '004');
+INSERT INTO locker (status, customer_number) VALUES (false, '005');
+INSERT INTO locker (status, customer_number) VALUES (false, '007');
 INSERT INTO locker (status, customer_number) VALUES (false, '008');
 INSERT INTO locker (status, customer_number) VALUES (false, '009');
 INSERT INTO locker (status, customer_number) VALUES (false, '010');
